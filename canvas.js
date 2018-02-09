8909a5ed3f0896a5fa3023cf9a927a6dfdbbbc35
imahdi2000 <imahdi@stuy.edu>
2018-02-08 23:48:33 -0500

    dvd

--- canvas.js
+++ canvas.js
@@ -15,11 +15,11 @@ var clearC = function(){
 
 var stop = function(){
   window.cancelAnimationFrame(requestID);
-  c.addEventListener("click",reset);
 };
 
 var run = function(){
  stop();
+ reset();
  var drawC = function(){
    clearC();
    context.beginPath();
@@ -37,22 +37,75 @@ var run = function(){
      x--;
    }
    context.fill();
-   context.stroke();
+  // context.stroke();
    requestID = window.requestAnimationFrame(  drawC );
-   console.log(requestID);
+   //console.log(requestID);
  };
  drawC();
-
 };
 
 var reset = function(){
   s = 0;
   x = 0;
-  run();
-}
+};
+
+//---------------------------------------------
+var x1 = 250;
+var y1 = 200;
+var touchx = 1;
+var touchy = 1;
+
+var reset2 = function(){
+   x1 = 250;
+   y1 = 200;
+   touchx = 1;
+   touchy = 1;
+};
+
+var edgeTouchx = function(x1){
+  if (x1 > 490){
+    touchx = 0;
+  }
+  if (x1 < -5){
+    touchx = 1;
+  }
+  //console.log(touchx);
+};
+
+var edgeTouchy = function(y1){
+  if (y1 > 490){
+    touchy = 0;
+  }
+  if (y1 < -5){
+    touchy = 1;
+  }
+};
 
 
-c.addEventListener("click",run);
-//st.addEventListener("click",stop);
+var run2 = function(){
+  stop();
+  reset2();
+  var drawR = function(){
+    clearC();
+    edgeTouchy(y1);
+    edgeTouchx(x1);
+    if (touchx == 1){
+      x1 = x1 + (Math.random() * 3);
+    }
+    if ( touchx == 0){
+      x1 = x1 - (Math.random() * 3);
+    }
+    if (touchy == 1){
+      y1 = y1 + (Math.random() * 2);
+    }
+    if (touchy == 0){
+      y1 = y1 - (Math.random() * 2);
+    }
+    //console.log(x1);
+    context.beginPath();
+    context.fillRect(x1,y1,20,20);
+    requestID = window.requestAnimationFrame(  drawR );
+  };
 
- //----------------------------------
+  drawR();
+};
